da1d4912420210c303541ac7f58750122f9c68ef
Ray <ray.gherman@amido.com>
2022-11-07 14:16:10 +0000

    (root): make calls async

--- Integration.Tests/Framework/Api/Base/IApiHelper.cs
+++ Integration.Tests/Framework/Api/Base/IApiHelper.cs
@@ -4,8 +4,8 @@ namespace Framework.Api.Base
 {
     public interface IApiHelper<T> where T : class
     {
-        public CoreApiResponse<T> GetSingle(string endpointPath, params string[] queryParameters);
-        public CoreApiResponse<IEnumerable<T>> GetMultiple(string endpointPath, params string[] queryParameters);
-        public CoreApiResponse<T> Post(string endpointPath, object request);
+        public Task<CoreApiResponse<T>> GetSingle(string endpointPath, params string[] queryParameters);
+        public Task<CoreApiResponse<IEnumerable<T>>> GetMultiple(string endpointPath, params string[] queryParameters);
+        public Task<CoreApiResponse<T>> Post(string endpointPath, object request);
     }
 }
