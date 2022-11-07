bdb034241062a7cc78c629c48bb2fff4fae9a1bf
Ray <ray.gherman@amido.com>
2022-11-07 14:12:25 +0000

    (root)~changes to core api bits

---
+++ Integration.Tests/Framework/Api/Base/IApiHelper.cs
@@ -0,0 +1,11 @@
+﻿using Framework.Api.Response;
+
+namespace Framework.Api.Base
+{
+    public interface IApiHelper<T> where T : class
+    {
+        public CoreApiResponse<T> GetSingle(string endpointPath, params string[] queryParameters);
+        public CoreApiResponse<IEnumerable<T>> GetMultiple(string endpointPath, params string[] queryParameters);
+        public CoreApiResponse<T> Post(string endpointPath, object request);
+    }
+}
---
+++ Integration.Tests/Framework/Api/Response/CoreApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Framework.Api.Response
+{
+    public class CoreApiResponse<T> where T : class
+    {
+        public bool Success { get; set; }
+        public HttpStatusCode Code { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
