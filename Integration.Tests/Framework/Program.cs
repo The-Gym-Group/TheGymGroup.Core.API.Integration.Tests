6f88aae027bff3b5461c0f8bbd5287453cb1f476
Ray <ray.gherman@amido.com>
2022-12-01 17:58:26 +0000

    +extra configuration options

--- Integration.Tests/Framework/Program.cs
+++ Integration.Tests/Framework/Program.cs
@@ -3,6 +3,7 @@ using Flurl.Http.Configuration;
 using Framework.Api;
 using Framework.Api.Base;
 using Framework.Properties.Constants;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 #region FlurlDependencies
@@ -10,7 +11,11 @@ builder.Services.AddSingleton<IFlurlClientFactory, DefaultFlurlClientFactory>();
 #endregion
 var app = builder.Build();
 ApiServerConfiguration.ROOT_URL = builder.Configuration["Configuration:ROOT_URL"];
+ApiServerConfiguration.ENVIRONMENT = builder.Configuration["Configuration:ENVIRONMENT"];
+ApiServerConfiguration.ConfigurationManager = builder.Configuration;
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+public partial class Program { }
