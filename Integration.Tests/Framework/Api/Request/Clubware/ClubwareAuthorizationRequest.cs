ec41e807978ec258e511de5f1a334164d7f8f1eb
Ray <ray.gherman@amido.com>
2022-12-01 17:58:07 +0000

    ~add extra requests/responses

---
+++ Integration.Tests/Framework/Api/Request/Clubware/ClubwareAuthorizationRequest.cs
@@ -0,0 +1,23 @@
+﻿using Framework.Properties.Constants;
+
+namespace Framework.Api.Request.Clubware
+{
+    public class ClubwareAuthorizationRequest
+    {
+        public ClubwareAuthorizationRequest()
+        {
+
+        }
+        public ClubwareAuthorizationRequest(string appId, string secret, string refreshToken)
+        {
+            AppID = appId;
+            Secret = secret;
+            RefreshToken = refreshToken;
+        }
+
+        public string AppID { get; set; } = ApiServerConfiguration.ConfigurationManager["Configuration:Clubware:AppID"];
+        public string Secret { get; set; } = ApiServerConfiguration.ConfigurationManager["Configuration:Clubware:Secret"];
+        public string RefreshToken { get; set; } = ApiServerConfiguration.ConfigurationManager["Configuration:Clubware:RefreshToken"];
+
+    }
+}
