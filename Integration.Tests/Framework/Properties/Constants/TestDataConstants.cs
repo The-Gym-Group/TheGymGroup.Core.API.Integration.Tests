0e13a9a7f63c792fb55b143dfbdbd7ae660300a9
Ray <ray.gherman@amido.com>
2023-01-03 16:08:39 +0000

    +const string for invalid guid

---
+++ Integration.Tests/Framework/Properties/Constants/TestDataConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Framework.Properties.Constants
+{
+    public static class TestDataConstants
+    {
+        public const string NO_DATA_GUID_FORMAT = "00000000-0000-0000-0000-000000000000";
+    }
+}
